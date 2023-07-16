7edd3a5e559a79afa038c5928815fb16f97440d9
Julio Santacruz <julio.santacruzh@gmail.com>
2023-07-15 19:48:08 -0700

    se agregaron unidades para uso en formularios

---
+++ src/utils/SelectInputOptions.ts
@@ -0,0 +1,54 @@
+const Unidad= [
+    // Unidades de tiempo
+    {name:'Jornal', simbol:'Jor', category:'Tiempo'},
+    {name:'Hora', simbol:'Hr', category:'Tiempo'},
+    {name:'Semana', simbol:'sem', category:'Tiempo'},
+    {name:'Mes', simbol:'mes', category:'Tiempo'},
+    {name:'Anio', simbol:'anio', category:'Tiempo'},
+
+
+    // Unidades de Distancia
+    {name:'Milimetro', simbol:'mm', category:'Longitud'},
+    {name:'Centimetro', simbol:'cm', category:'Longitud'},
+    {name:'Metro', simbol:'mL', category:'Longitud'},
+    {name:'Pulgada', simbol:'in', category:'Longitud'},
+    {name:'Pie', simbol:'ft', category:'Longitud'},
+    {name:'Yarda', simbol:'yr', category:'Longitud'},
+
+    // Unidades de Area
+    {name:'Milimetro Cuadrado', simbol:'mm2', category:'Area'},
+    {name:'Centimetro Cuadrado', simbol:'cm2', category:'Area'},
+    {name:'Metro Cuadrado', simbol:'m2', category:'Area'},
+    {name:'Hectarea', simbol:'ha', category:'Area'},
+    {name:'Pulgada Cuadrada', simbol:'in2', category:'Area'},
+    {name:'Pie Cuadrada', simbol:'ft2', category:'Area'},
+    {name:'Yarda Cuadrada', simbol:'yr2', category:'Area'},
+    {name:'Acre', simbol:'ac', category:'Area'},
+
+    // Unidades de Volumen
+    {name:'Milimetro Cubico', simbol:'mm3', category:'Volumen'},
+    {name:'Centimetro Cubico', simbol:'cm3', category:'Volumen'},
+    {name:'Litro', simbol:'lt', category:'Volumen'},
+    {name:'Metro Cubico', simbol:'m3', category:'Volumen'},
+    {name:'Galon', simbol:'gal', category:'Volumen'},
+    {name:'Cubeta', simbol:'cu', category:'Volumen'},
+    {name:'Pulgada Cubica', simbol:'in3', category:'Volumen'},
+    {name:'Pie Cubica', simbol:'ft3', category:'Volumen'},
+    {name:'Yarda Cubica', simbol:'yr3', category:'Volumen'},
+
+
+    // Unidades de Peso
+    {name:'Gramo', simbol:'gr', category:'Peso'},
+    {name:'Kilogramo', simbol:'kg', category:'Peso'},
+    {name:'Tonelada', simbol:'tn', category:'Peso'},
+    {name:'Onza', simbol:'oz', category:'Peso'},
+    {name:'Libra', simbol:'lb', category:'Peso'},
+
+
+    // Unidades piezas
+    {name:'Pieza', simbol:'pza', category:'Pieza'},
+    {name:'Caja', simbol:'cja', category:'Pieza'},
+    {name:'Lote', simbol:'lote', category:'Pieza'},
+
+
+] 
