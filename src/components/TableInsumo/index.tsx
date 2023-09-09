66e6d57f18924b3ac11f56794a732aca8c1b1927
Julio Santacruz <julio.santacruzh@gmail.com>
2023-09-09 11:19:10 -0700

    se agrego tabs en insumos

--- src/components/TableInsumo/index.tsx
+++ src/components/TableInsumo/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Insumo } from "../../types/Insumo";
 import AnyIcon from "../AnyIcon";
 import editIcon from "../../assets/icons/bx-edit.svg";
@@ -8,6 +8,7 @@ import { useInsumoStore } from "../../store/projectStore";
 import { useUxStore } from "../../store/uxStore";
 import "./TableInsumo.scss";
 import { setFormat } from "../../utils/CurrencyFormat";
+import { CategoriasInsumos } from "../../utils/SelectInputOptions";
 
 type Props = { insumosData: Insumo[]; addInputInsumo?: (id: string) => void };
 
@@ -33,80 +34,114 @@ export default function TableInsumo({ insumosData, addInputInsumo }: Props) {
     openModalFormInsumo(true);
   };
 
+  const [filter, setFilter] = useState("todos");
+  const filtrarCategorias = (data: Insumo[], categoria: string) => {
+    let arrData: Insumo[] = [];
+    if (filter === "todos") {
+      arrData = data;
+    } else {
+      arrData = data.filter((insumo) => insumo.categoria === categoria);
+    }
+    return arrData;
+  };
+  const tabClassName =(category?:string)=>{
+    if(filter===category){
+      return 'tab active'
+    }else{
+      return 'tab'
+    }
+  }
+
   return (
-    <table className="tableInsumo">
-      <thead>
-        <tr>
-          <td>Clave</td>
-          <td>Descripcion</td>
-          <td>Unidad</td>
-          <td>Precio</td>
-          <td>Categoria</td>
-          <td>Actions</td>
-        </tr>
-      </thead>
-      <tbody>
-        {data.length > 0 ? (
-          data?.map((insumo: Insumo) => {
-            return (
-              <tr key={insumo.id}>
-                <td className="clave">{insumo.clave}</td>
-                <td className="descripcion">{insumo.descripcion}</td>
-                <td className="unidad">{insumo.unidad}</td>
-                <td className="precio">{setFormat(insumo.precio)}</td>
-                <td className="categoria">{insumo.categoria}</td>
-                <td className="actions">
-                  {addInputInsumo ? (
-                    <>
-                      <a onClick={() => handleEdit(insumo)}>
-                        <AnyIcon
-                          className={"icon"}
-                          iconSrc={editIcon}
-                          iconWidth={14}
-                          iconHeight={14}
-                        />
-                      </a>
-                      |
-                      <button
-                        type="button"
-                        onClick={() => addInputInsumo(insumo.id)}
-                      >
-                        <AnyIcon
-                          iconSrc={addIcon}
-                          iconWidth={14}
-                          iconHeight={14}
-                        />
-                        agregar
-                      </button>
-                    </>
-                  ) : (
-                    <>
-                      <a onClick={() => handleEdit(insumo)}>
-                        <AnyIcon
-                          className={"icon"}
-                          iconSrc={editIcon}
-                          iconWidth={14}
-                          iconHeight={14}
-                        />
-                      </a>
-                      |
-                      <a onClick={() => handleDelete(insumo.id)}>
-                        <AnyIcon
-                          iconSrc={deleteIcon}
-                          iconWidth={14}
-                          iconHeight={14}
-                        />
-                      </a>
-                    </>
-                  )}
-                </td>
-              </tr>
-            );
-          })
-        ) : (
-          <p>no data</p>
-        )}
-      </tbody>
-    </table>
+    <>
+      <div className="tabsFiltred">
+        <button type="button" className={filter==='todos'?"tab active":"tab"} onClick={() => setFilter("todos")}>
+          Todos
+        </button>
+        {CategoriasInsumos.map((category) => {
+
+          return (
+            <button type="button" className={tabClassName(category.name)} onClick={() => setFilter(category.name)}>
+              {category.name}
+            </button>
+          );
+        })}
+      </div>
+      <div className="insumosTable">
+        <table className="tableInsumo">
+          <thead>
+            <tr>
+              <td>Clave</td>
+              <td>Descripcion</td>
+              <td>Unidad</td>
+              <td>Precio</td>
+              <td>Categoria</td>
+              <td>Actions</td>
+            </tr>
+          </thead>
+          <tbody>
+            {data.length > 0 ? (
+              filtrarCategorias(data, filter)?.map((insumo: Insumo) => {
+                return (
+                  <tr key={insumo.id}>
+                    <td className="clave">{insumo.clave}</td>
+                    <td className="descripcion">{insumo.descripcion}</td>
+                    <td className="unidad">{insumo.unidad}</td>
+                    <td className="precio">{setFormat(insumo.precio)}</td>
+                    <td className="categoria">{insumo.categoria}</td>
+                    <td className="actions">
+                      {addInputInsumo ? (
+                        <>
+                          <a onClick={() => handleEdit(insumo)}>
+                            <AnyIcon
+                              className={"icon"}
+                              iconSrc={editIcon}
+                              iconWidth={14}
+                              iconHeight={14}
+                            />
+                          </a>
+                          |
+                          <a
+                            type="button"
+                            onClick={() => addInputInsumo(insumo.id)}
+                          >
+                            <AnyIcon
+                              iconSrc={addIcon}
+                              iconWidth={14}
+                              iconHeight={14}
+                            />
+                          </a>
+                        </>
+                      ) : (
+                        <>
+                          <a onClick={() => handleEdit(insumo)}>
+                            <AnyIcon
+                              className={"icon"}
+                              iconSrc={editIcon}
+                              iconWidth={14}
+                              iconHeight={14}
+                            />
+                          </a>
+                          |
+                          <a onClick={() => handleDelete(insumo.id)}>
+                            <AnyIcon
+                              iconSrc={deleteIcon}
+                              iconWidth={14}
+                              iconHeight={14}
+                            />
+                          </a>
+                        </>
+                      )}
+                    </td>
+                  </tr>
+                );
+              })
+            ) : (
+              <p>no data</p>
+            )}
+          </tbody>
+        </table>
+      </div>
+    </>
   );
 }
