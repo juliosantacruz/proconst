22568400d25b4aa8bac4aefd47dc6aafdc350170
Julio Santacruz <julio.santacruzh@gmail.com>
2023-09-11 10:22:18 -0700

    test hashroutes

--- src/main.tsx
+++ src/main.tsx
@@ -3,14 +3,14 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import MainLayout from "./layout/MainLayout.tsx";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <MainLayout>
         <App />
       </MainLayout>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
