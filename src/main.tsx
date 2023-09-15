3c10f71c82a061162bb6c1940c5ecd476f35ec16
Julio Santacruz <julio.santacruzh@gmail.com>
2023-09-14 23:27:58 -0700

    se cambio a HashRouter

--- src/main.tsx
+++ src/main.tsx
@@ -3,15 +3,15 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.scss";
 import MainLayout from "./layout/MainLayout.tsx";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <MainLayout >
         <App />
       </MainLayout>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
  
